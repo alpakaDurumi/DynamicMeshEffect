c5013eae16dea55e3fba359968a3f92499078849
alpakaDurumi <durams@naver.com>
2024-10-19 22:11:35 +0900

    Change default value of Material

--- DME/Material.h
+++ DME/Material.h
@@ -7,9 +7,8 @@ using DirectX::XMFLOAT3;
 struct Material {
     XMFLOAT3 ambient = { 0.1f, 0.1f, 0.1f };
     float shininess = 10.0f;
-    XMFLOAT3 diffuse = { 1.0f, 1.0f, 1.0f };
+    XMFLOAT3 diffuse = { 0.7f, 0.7f, 0.7f };
     float padding1;
-    XMFLOAT3 specular = { 1.0f, 1.0f, 1.0f };
+    XMFLOAT3 specular = { 0.5f, 0.5f, 0.5f };
     float padding2;
 };
-
